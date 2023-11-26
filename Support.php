d9f68f48e1331f8aebe555e40c29ae12e2a03072
EngAndy <143444459+EngAndy@users.noreply.github.com>
2023-11-26 19:12:35 +0300

    Repair to some pages

---
+++ Support.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="contact">
+        <div class="content">
+            <h1>Contact Us</h1>
+            <p>This platform provides a convenient and efficient way to get in touch with the experts and receive prompt assistance. Whether it's seeking advice for community projects or seeking answers to questions about social initiatives.
+            </p>
+        </div>
+        <div class="container">
+            <div class="box">
+                <div class="icon">
+                    <i class="fa-solid fa-map-marker" ></i>
+                </div>
+                <div class="text">
+                    <h3>Address</h3>
+                    <p>Mai-mihiu Road, <br>Nairobi Kenya,<br>52628-00200</p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="icon">
+                    <i class="fas fa-phone"></i>
+                </div>
+                <div class="text">
+                    <h3>Phone</h3>
+                    <p>+254796170388</p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="icon">
+                    <i class="fas fa-envelope"></i>
+                </div>
+                <div class="text">
+                    <h3>Email</h3>
+                    <p>andrew.chelimo@gmail.co</p>
+                </div>
+            </div>
+            <div class="contactForm">
+                <form action="" method="post">
+                    <h2> </h2>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ services.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <title>Services page</title>
+</head>
+<body>
+    <div class="section">
+        <div class="title">
+            <h1>Our Services</h1>
+        </div>
+        <div class="services">
+            <div class="card">
+                <div class="icon">
+                    <i class="fas fa-calendar"> </i>
+                </div>
+                <h2>Planning</h2>
+                <p>we welcome you to try plan with our in mind as we connect you toinfluencers according to the industry
+                </p>
+                <a href=""class="button"> Read More</a>
+            </div>
+            <div class="card">
+                <div class="icon">
+                    <i class="fas fa-wrench"> </i>
+                </div>
+                <h2>Rennovation</h2>
+                <p>we welcome you to try plan with our in mind as we connect you toinfluencers according to the industry
+                </p>
+                <a href=""class="button"> Read More</a>
+            </div>
+            <div class="card">
+                <div class="icon">
+                    <i class="fas fa-handshake"> </i>
+                </div>
+                <h2>Planning</h2>
+                <p>we welcome you to try plan with our in mind as we connect you toinfluencers according to the industry
+                </p>
+                <a href=""class="button"> Read More</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
